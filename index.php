99a3d7d7cd2cfd5928faa35ce419d3a757f99615
Axkrm <160167876+Steveksteve@users.noreply.github.com>
2026-01-30 15:12:44 +0100

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-echo "bonjour";
+
+bridge.js
+1 KB
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dcument</title>
+    <script src="main.js"></script>
+</head>
+
+<body>
+
+    <div>
+        <p>
+            Température = <span id="temp">00</span> <span id="unit">°</span>
+        </p>
+
+        <p>
+            Humidity = <span id="humidity">00.00</span> %
+        </p>
+
+    </div>
+
+
+</body>
+
+</html>
