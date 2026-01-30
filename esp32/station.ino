6bcf51754ddfe2e660d5c0b6210dccbf7b1030a8
Axkrm <160167876+Steveksteve@users.noreply.github.com>
2026-01-30 16:14:47 +0100

    Update station.ino

--- esp32/station.ino
+++ esp32/station.ino
@@ -2,73 +2,90 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
-/* ================= WIFI ================= */
-const char *WIFI_SSID = "s22ultraYounes";
-const char *WIFI_PASS = "12341234";
+// Infos de connection Wifi
 
-/* ================= MQTT ================= */
-const char *MQTT_HOST  = "captain.dev0.pandor.cloud";
+const char* WIFI_SSID = "s22ultraYounes";
+const char* WIFI_PASS = "12341234";
+
+// Infos MQTT
+
+const char* MQTT_HOST = "captain.dev0.pandor.cloud";
 const uint16_t MQTT_PORT = 1884;
-const char *MQTT_TOPIC = "classroom/YounesBenaggoun";
+const char* MQTT_TOPIC = "classroom/YounesBenaggoun";
 
-const char *MQTT_USER = "";
-const char *MQTT_PASS = "";
-const char *DEVICE_ID = "esp32-Younes";
+// Si on avait de l'auth sur le MQTT 
+const char* MQTT_USER = "";
+const char* MQTT_PASS = "";
 
-/* ================= TIMING ================= */
-const uint32_t PUBLISH_INTERVAL_MS = 5000;
-unsigned long lastPublishMs = 0;
-uint32_t seq = 1;
+const char* DEVICE_ID = "esp32-Younes";
+uint32_t seq = 42;
+const uint32_t baseTs = 1767828437;
+const uint32_t publishIntervalMs = 5000;
 
-/* ================= HARDWARE ================= */
-const int BUTTON_PIN = 2;
-const int LED_GREEN  = 4;   // Celsius
-const int LED_RED    = 5;   // Fahrenheit
-
-/* Simulation pins (pas de DHT en mode simulation) */
-const int TEMP_PIN     = 22;
-
-
-/* ================= STATES ================= */
-bool degreeModeCelsius = true;
-bool lastButtonState = 0;
-
-/* Anti-rebond */
-unsigned long lastDebounceTime = 0;
-const unsigned long debounceDelay = 200;
-
-/* ================= CLIENTS ================= */
 WiFiClient wifiClient;
 PubSubClient mqtt(wifiClient);
 
-/* ================= WIFI ================= */
+unsigned long lastPublishMs = 0;
+
+/////////////////
+///////////////
+
+const int Button_pin = 2;
+bool currentButtonState = 0;
+bool lastButtonState = 0;
+
+const int red_led = 5;
+const int green_led = 4;
+bool green_led_state = 0;
+
+int temperaturePin = 22;
+
+
+//float humidity = 0;
+float temperature = 0;
+float finaleTemperature = 0;
+
+
+bool degreeMode = 0;
+//////////////////////////
+////////////////////////////
+
 void connectWiFi() {
+  Serial.print("[WIFI] Connecting to "); // Serial.print => Pas de retour de ligne
+  Serial.println(WIFI_SSID); // Serial.println => Retour de ligne
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-  Serial.print("[WIFI] Connecting");
-  while (WiFi.status() != WL_CONNECTED) {
+  while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println(" OK");
-  Serial.print("[WIFI] IP: ");
+
+  Serial.println("OK.");
+
+  Serial.print("WiFi: connected, IP=");
   Serial.println(WiFi.localIP());
+  
 }
 
-/* ================= MQTT ================= */
 void connectMQTT() {
-  mqtt.setServer(MQTT_HOST, MQTT_PORT);
+  mqtt.setServer(MQTT_HOST,MQTT_PORT);
+  
+  while(!mqtt.connected()) {
+    String clientId = String("esp32-") + String((uint32_t)ESP.getEfuseMac(), HEX);
 
-  while (!mqtt.connected()) {
-    String clientId = "esp32-" + String((uint32_t)ESP.getEfuseMac(), HEX);
-    Serial.print("[MQTT] Connecting as ");
+    Serial.print("MQTT: connecting as ");
     Serial.println(clientId);
 
-    if (mqtt.connect(clientId.c_str(), MQTT_USER, MQTT_PASS)) {
+    bool ok;
+
+    ok = mqtt.connect(clientId.c_str(), MQTT_USER, MQTT_PASS);
+
+    if(ok) {
       Serial.println("[MQTT] Connected");
     } else {
-      Serial.print("[MQTT] Failed, rc=");
+      Serial.print("[MQTT] failed, rc=");
       Serial.print(mqtt.state());
       Serial.println(" retry in 2s");
       delay(2000);
@@ -76,82 +93,120 @@ void connectMQTT() {
   }
 }
 
-/* ================= BUTTON / LED ================= */
-void handleButton() {
-  bool currentState = digitalRead(BUTTON_PIN);
+void publishDummyTelemetry(float tempC, float humPct , bool degreeMode ) {
+  //float tempC = 22.4f + (float)random(-15,16) / 10.0f;
+  //float humPct = 42.67f + (float)random(-30,31) / 10.0f;
+  int batteryPct = 100;
 
-  if (currentState == LOW && lastButtonState == HIGH) {
-    if (millis() - lastDebounceTime > debounceDelay) {
-      degreeModeCelsius = !degreeModeCelsius;
-
-      digitalWrite(LED_GREEN, degreeModeCelsius ? HIGH : LOW);
-      digitalWrite(LED_RED,   degreeModeCelsius ? LOW  : HIGH);
-
-      lastDebounceTime = millis();
-    }
-  }
-  lastButtonState = currentState;
-}
-
-/* ================= MQTT PUBLISH ================= */
-void publishTelemetry(float temperature, float humidity, bool degreeModeCelsius) {
   StaticJsonDocument<256> doc;
-
-  doc["deviceId"] = DEVICE_ID;
-  doc["seq"] = seq++;
-  doc["temperature"] = temperature;
-  doc["humidity"] = humidity;
-  doc["unit"] = degreeModeCelsius;
+  JsonObject t = doc.to<JsonObject>();
+  t["deviceId"] = DEVICE_ID;
+  t["ts"] = baseTs + (millis() / 1000);
+  t["seq"] = seq++;
+  t["tempC"] = tempC;
+  t["humPct"] = humPct;
+  t["degreeMode"] = degreeMode;
+  t["batteryPct"] = batteryPct;
 
   char payload[256];
-  serializeJson(doc, payload);
+  size_t n = serializeJson(doc, payload, sizeof(payload));
 
-  bool ok = mqtt.publish(MQTT_TOPIC, payload);
-  Serial.print("[MQTT] Publish: ");
+  bool ok = mqtt.publish(MQTT_TOPIC, payload, n);
+  Serial.print("[MQTT] Publish to ");
+  Serial.print(MQTT_TOPIC);
+  Serial.print(" ... ");
   Serial.println(ok ? payload : "FAILED");
+
+}
+///////////////////////////////////////
+////////////////////////////////////////
+
+float fahrenheitToCelsius(float fahren){
+  return (fahren - 32) / 1.8;
 }
 
-/* ================= SETUP ================= */
+float getTemp(){
+  float calculatedTemp = 0;
+  
+  temperature = digitalRead(temperaturePin);
+  
+  currentButtonState = digitalRead(Button_pin);
+  
+  if(currentButtonState != lastButtonState){
+ 		lastButtonState = currentButtonState;
+    	if(currentButtonState == 0){
+      		//Serial.println("keyDown ");
+    	}else{
+      		//Serial.println("KeyUp ");
+          if(green_led_state){
+            digitalWrite(green_led, HIGH);
+            digitalWrite(red_led, LOW);
+            green_led_state = 0;
+            degreeMode = 1;
+
+            Serial.println(finaleTemperature);
+          }else{
+            digitalWrite(green_led, LOW);
+            digitalWrite(red_led, HIGH);
+            green_led_state = 1;
+            degreeMode = 0;
+          } 
+    	}
+  	}
+  
+  if(degreeMode){
+        calculatedTemp = fahrenheitToCelsius(temperature);
+  }else{
+    	calculatedTemp = temperature;
+  }
+  return calculatedTemp;
+  
+}
+/////////////////////////////////////
+/////////////////////////////////
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
-
-  /* État initial : Celsius */
-  digitalWrite(LED_GREEN, HIGH);
-  digitalWrite(LED_RED, LOW);
-
-  randomSeed(analogRead(0));
-
   connectWiFi();
   connectMQTT();
+  // put your setup code here, to run once:
+
+  /////////////////
+  pinMode(Button_pin, INPUT_PULLUP);
+  pinMode(temperaturePin,INPUT);
+  pinMode(humidityPin, INPUT);
+  ////////////////
+
 }
 
-/* ================= LOOP ================= */
 void loop() {
-  if (WiFi.status() != WL_CONNECTED) connectWiFi();
-  if (!mqtt.connected()) connectMQTT();
+  if(WiFi.status() != WL_CONNECTED) {
+    connectWiFi();
+  }
+  if(!mqtt.connected()) {
+    connectMQTT();
+  }
+
   mqtt.loop();
 
-  handleButton();
 
   unsigned long now = millis();
-  if (now - lastPublishMs >= PUBLISH_INTERVAL_MS) {
+
+  ////////////////////////////////////////
+
+  humidity = analogRead(humidityPin);
+  finaleTemperature = getTemp();
+///////////////////////////////////////////
+
+  if(now - lastPublishMs >= publishIntervalMs) {
+
+
     lastPublishMs = now;
-
-    /* ===== MODE SIMULATION ===== */
-    float tempC = random(180, 300) / 10.0; // 18.0 → 30.0 °C
-    float humidity = random(300, 700) / 10.0; // 30 → 70 %
-
-    float tempToSend = tempC;
-    if (!degreeModeCelsius) {
-      tempToSend = tempC * 1.8 + 32.0; // °F
-    }
-
-    publishTelemetry(tempToSend, humidity);
+    publishDummyTelemetry(finaleTemperature, humidity, degreeMode );
   }
+
 }
+
 
